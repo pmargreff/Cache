fe4bea1bad9d81d49d8620742ceeaaf7fb216878
pmargreff <pablo_margreff@hotmail.com>
2015-10-16 10:12:53 -0300

    Distributing corrected on associative methods

--- Cache.java
+++ Cache.java
@@ -41,7 +41,7 @@ public class Cache {
                 directMapped(address);
             } else if (nSets == 1) {
                 fullyAssociative(address);
-            } else {
+            } else if (nSets > 1 && assoc > 1) {
                 setAssociative(address);
             }
         } else {
@@ -49,7 +49,6 @@ public class Cache {
         }
     }
 
-    
     public void directMapped(int address) {
         int tag; //Tag on that will be on cache memory
         int blockIndex; //block line that will be changed
@@ -74,54 +73,70 @@ public class Cache {
     }
 
     public void setAssociative(int address) {
+        /**
+         * Only do a div address to bSize, if was need get and set memory data
+         * is only necessary do a loop and set or get cells in a Block _bSize
+         * times
+         */
+        address /= _bSize;
 
-//        int set;
-//        int i;
-//        set = address % _nSets;
-//        //for(i=0;i<_cellsPerBlock && ( _sets.get(set).getBlocks().get(i).getTag()!=((int)address/_nSets) && _sets.get(set).getBlocks().get(i).getValidate()==true); i++);
-//        i = 0;
-//        while (i < _associative && (_sets.get(set).getBlocks().get(i).getTag() != ((int) address / _nSets))) {
-//            i++;
-//        }
-//
-//        if (i >= _associative) {
-//            _stats.addMiss();
-//            for (i = 0; i < _associative && _sets.get(set).getBlocks().get(i).getValidate() == true; i++);
-//            if (i >= _associative) {
-//                Random place = new Random();
-//                int index = place.nextInt(_associative);
-//                _sets.get(set).getBlocks().get(index).setValidate(true);
-//                _sets.get(set).getBlocks().get(index).setTag((int) address / _nSets);
-//            } else {
-//                _sets.get(set).getBlocks().get(i).setValidate(true);
-//                _sets.get(set).getBlocks().get(i).setTag((int) address / _nSets);
-//            }
-//        } else {
-//            _stats.addHit();
-//        }
+        int associativeIndex = address % _associative;
+        int tag = (int) address / _associative;
+        boolean hasFound = false;
+        boolean hasOccupied = true;
+
+        for (int i = 0; i < _nSets; i++) {
+            if ((_sets.get(associativeIndex).getBlocks().get(i).getValidate()) && (_sets.get(associativeIndex).getBlocks().get(i).getTag() == tag)) {
+                _stats.addHit();
+                hasFound = true;
+                break;
+            }
+        }
+
+        if (!hasFound) {
+            _stats.addMiss();
+            for (int i = 0; i < _nSets; i++) {
+                if (!_sets.get(associativeIndex).getBlock(i).getValidate()) {
+                    _sets.get(associativeIndex).getBlock(i).setValidate(true);
+                    _sets.get(associativeIndex).getBlock(i).setTag(tag);
+                    hasOccupied = true;
+                    break; //if find value get out a loop           
+                }
+                hasOccupied = false;
+            }
+        }
+
+        if (!hasOccupied) {
+
+            Random random = new Random();
+            int index = random.nextInt(_nSets);
+            _sets.get(associativeIndex).getBlock(index).setValidate(true);
+            _sets.get(associativeIndex).getBlock(index).setTag(tag);
+
+        }
+
     }
 
     public void fullyAssociative(int address) {
+        /**
+         * Only do a div address to bSize, if was need get and set memory data
+         * is only necessary do a loop and set or get cells in a Block _bSize
+         * times
+         */
+        address /= _bSize;
         int tag;
         boolean hasFound = false;
-        boolean hasOccupied = false;
-        int index;
-        
-        /**
-         * Only do a division to address, if was need get and set memory data
-         * is only necessary do a loop and set or get cells in a Block _bSize times
-         */
-        address /= _bSize; 
+        boolean hasOccupied = true;
 
 //          search for adress in cache
         for (int i = 0; i < _associative; i++) {
-            if ((_sets.get(i).getBlock(0).getValidate() == true) && (_sets.get(i).getBlock(0).getTag() == address)) {
+            if ((_sets.get(i).getBlock(0).getValidate()) && (_sets.get(i).getBlock(0).getTag() == address)) {
                 _stats.addHit();
                 hasFound = true;
                 break; //if find value get out a loop
             }
         }
-
+        
         //if don't find adress count hit and replace, first search by a empty 
         //place, after it set a block in a random index
         if (!hasFound) {
@@ -133,14 +148,14 @@ public class Cache {
                     hasOccupied = true;
                     break; //if find value get out a loop
                 }
+                hasOccupied = false; 
             }
         }
         if (!hasOccupied) {
             Random random = new Random();
-            index = random.nextInt(_associative);
+            int index = random.nextInt(_associative);
             _sets.get(index).getBlock(0).setValidate(true);
             _sets.get(index).getBlock(0).setTag(address);
-
         }
     }
 }
--- Processor.java
+++ Processor.java
@@ -46,15 +46,15 @@ public class Processor {
         this._requestType = new ArrayList<>(); //Init the comand list (Read or Write)
 
         this._nSetsL1D = 1;
-        this._assocL1D = 128;
+        this._assocL1D = 64;
         this._nSetsL1I = 1;
-        this._assocL1I = 32;
+        this._assocL1I = 1;
 //        this._nSetsL2U = 1;
 //        this._assocL2U = 1;
 
-        this._bSizeL1D = 6;
-        this._bSizeL1I = 1;
-        this._splitAddress = 500;
+        this._bSizeL1D = 4;
+        this._bSizeL1I = 4;
+        this._splitAddress = 1000;
 
         this._cacheL1D = new Cache(this._nSetsL1D, this._assocL1D, this._bSizeL1D); //Init cache with split address plus 1
         this._cacheL1I = new Cache(this._nSetsL1I, this._assocL1I, this._bSizeL1I); //Init cache with the rest of size
