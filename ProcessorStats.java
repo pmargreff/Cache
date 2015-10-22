7d0ab109e83738e41ff474939fee5f684443db71
pmargreff <pablo_margreff@hotmail.com>
2015-10-22 16:11:24 -0200

    All kinds of miss specifieds

--- ProcessorStats.java
+++ ProcessorStats.java
@@ -5,18 +5,39 @@
 public class ProcessorStats {
     private int _miss;
     private int _hit;
-
+    private int _capacity;
+    private int _compulsory;
+    private int _conflict;
+    private int _access;
+    
     public ProcessorStats() {
         this._hit = 0;
         this._miss = 0;
     }
 
-    public void addMiss(){
+    public void addConflictMiss(){
+        this._conflict++;
+        addMiss();
+    }
+    
+    public void addCompulsoryMiss(){
+        this._compulsory++;
+        addMiss();
+    }
+    
+    public void addCapacityMiss(){
+        this._capacity++;
+        addMiss();
+    }
+    
+    private void addMiss(){
         this._miss++;
+        this._access++;
     }
 
     public void  addHit(){
         this._hit++;
+        this._access++;
     }
 
     public int getMiss() {
@@ -26,5 +47,29 @@ public class ProcessorStats {
 
     public int getHit() {
         return _hit;
+    }
+
+    public int getCapacity() {
+        return _capacity;
+    }
+
+    public void setCapacity(int _capacity) {
+        this._capacity = _capacity;
+    }
+
+    public int getCompulsory() {
+        return _compulsory;
+    }
+
+    public void setCompulsory(int _compulsory) {
+        this._compulsory = _compulsory;
+    }
+
+    public int getConflict() {
+        return _conflict;
+    }
+
+    public void setConflict(int _conflict) {
+        this._conflict = _conflict;
     }
 }
